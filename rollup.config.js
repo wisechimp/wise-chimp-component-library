aa54e23ec737e735705af84148ed792b47f790f7
shicks1080 <stuart.hicks@gmail.com>
2022-03-13 11:39:49 +0100

    Adding ability to support css processing

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import typescript from "@rollup/plugin-typescript"
 import dts from "rollup-plugin-dts"
+import postcss from "rollup-plugin-postcss"
 
 const packageJson = require("./package.json")
 
@@ -23,6 +24,7 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
+      postcss(),
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
   },
@@ -30,5 +32,6 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "ems" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ]
