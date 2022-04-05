fd5842edfed49e97ed170e6c048a392fcd74e190
shicks1080 <stuart.hicks@gmail.com>
2022-04-05 15:04:29 +0200

    Adding a test for the button link

--- src/components/ButtonLink/ButtonLink.test.tsx
+++ src/components/ButtonLink/ButtonLink.test.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { screen, render, getByRole } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 import ButtonLink from './ButtonLink'
 
 describe("ButtonLink", () => {
   test("renders the ButtonLink component", () => {
     render(<ButtonLink target='https://skanestags.com' text='Stag Off' />)
+
+    screen.debug()
+
+    const buttonLink = screen.getByRole('link')
+    expect(buttonLink).toHaveAttribute('href', 'https://skanestags.com')
+  });
+
+  test("link has a href", () => {
+    render(<ButtonLink target='https://skanestags.com' text='Stag Off' />)
+
+    screen.debug()
+
+    const buttonLink = screen.getByRole('link')
+    expect(buttonLink).toHaveAttribute('href', 'https://skanestags.com')
   })
 })
